1ddf97b5ff786ffd9852e6bb9bc97f4d617b4551
b-zhu524 <b.zhu524@gmail.com>
2021-08-15 17:11:59 -0700

    first commit

--- fence_painting.py
+++ fence_painting.py
@@ -1,8 +1,20 @@
+import sys
+
+
 def find_union(a, b, c, d):
     startpoint = max(a, c)
     endpoint = min(b, d)
-    intersection_ = endpoint - startpoint
-    intersection = max(0, intersection_)
+    intersection = max(0, endpoint - startpoint)
     union = (b - a) + (d - c) - intersection
-
     return union
+
+
+if __name__ == '__main__':
+    sys.stdin = open("paint.in", "r")
+    sys.stdout = open("paint.out", "w")
+
+    a, b = map(int, input().split())
+    c, d = map(int, input().split())
+
+    print(find_union(a, b, c, d))
+
